c6841f22b5f7bd4550989778889cae20cfdb074b
KirillSmirnov <smirnowku@gmail.com>
2023-08-19 14:36:56 +0300

    Add dog entity

---
+++ src/main/java/flyway/model/Dog.java
@@ -0,0 +1,22 @@
+package flyway.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Dog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(min = 1, max = 100)
+    private String name;
+}
---
+++ src/main/resources/db/migration/V2__add_dog_entity.sql
@@ -0,0 +1,6 @@
+create table public.dog
+(
+    id   bigserial
+        primary key,
+    name varchar(100) not null
+);
