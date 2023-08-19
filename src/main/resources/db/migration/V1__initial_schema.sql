91f53276bd1539f7e04c86dbe42a8366fbcad774
KirillSmirnov <smirnowku@gmail.com>
2023-08-19 14:16:04 +0300

    Initial schema

---
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -0,0 +1,29 @@
+create table position
+(
+    id        uuid             not null
+        primary key,
+    latitude  double precision not null
+        constraint position_latitude_check
+            check ((latitude >= ('-90'::integer)::double precision) AND (latitude <= (90)::double precision)),
+    longitude double precision not null
+        constraint position_longitude_check
+            check ((longitude <= (180)::double precision) AND (longitude >= ('-180'::integer)::double precision))
+);
+
+create table venue
+(
+    id              bigserial
+        primary key,
+    approval_status varchar(255) not null
+        constraint venue_approval_status_check
+            check ((approval_status)::text = ANY
+                   ((ARRAY ['PENDING'::character varying, 'APPROVED'::character varying, 'REJECTED'::character varying])::text[])),
+    name            varchar(100) not null
+        constraint uk_9uyuoc3vs9lv1qpae6j8q40xe
+            unique,
+    position_id     uuid         not null
+        constraint uk_ovej8qot8ewep18t0uofumg4p
+            unique
+        constraint fk40r13e17m6arj0fn7ymr3n16f
+            references position
+);
